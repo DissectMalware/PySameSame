e682ea81cff23b4e96da93349920e6397898a42a
Malwrologist <aniakan@gmail.com>
2018-08-20 23:35:41 -0400

    Add convert_to_ascii function

--- samesame.py
+++ samesame.py
@@ -2,3 +2,7 @@ import english_confusables
 
 print(english_confusables.get_homograph("TEST test test Test"))
 
+print(english_confusables.convert_to_ascii("ŧѡɨŧŧеr for test"))
+
+print(english_confusables.convert_to_ascii("测试"))
+
